f336381b03a79e7e8c6feabd08d6800214886df9
Tigran Keshishyan <tigrankeshishyan1@gmail.com>
2022-12-06 16:09:06 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import {Routes, Route } from 'react-router-dom';
-import './App.scss';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 
+import './App.scss';
+
 function App() {
   return (
     <>
       <Routes>
-        <Route exact path='/' element={<Layout/>}>
-          <Route exact index element={<Home/>}/>
-          <Route exact path='about' element={<About/>}/>
-          <Route exact path='contact' element={<Contact/>}/>
+        <Route exact path='/' element={<Layout />}>
+          <Route exact index element={<Home />} />
+          <Route exact path='about' element={<About />} />
+          <Route exact path='contact' element={<Contact />} />
         </Route>
-      </Routes> 
+      </Routes>
     </>
   );
 }
--- src/Sidbar/index.js
+++ src/Sidbar/index.js
@@ -1,73 +1,73 @@
-import {Link, NavLink} from 'react-router-dom';
-import './index.scss';
+import { Link, NavLink } from 'react-router-dom';
 import LogoS from '.././assets/images/logo.jpg';
 import LogoSubtitle from '.././assets/images/logosub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome,faUser } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
+import './index.scss';
 
-const Sidbar = () => (
-   <div  className='nav-bar'>
-    <Link className='logo' to='/'>
-        <img src={LogoS} alt='logo' />
-        <img className='sub=logo' src={LogoSubtitle} alt='logo' />
-    </Link> 
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt='logo' />
+            <img className='sub=logo' src={LogoSubtitle} alt='logo' />
+        </Link>
         <nav>
-        <NavLink 
-            exact="true"
-            activeclassname="active"
-            to="/"
-        >
-            <FontAwesomeIcon icon={faHome} color="4d4d4d" />
-        </NavLink>
-        <NavLink
-            exact="true"
-            activeclassname="active"
-            className="about-link"
-            to="about"
-        >
-            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-            exact="true"
-            activeclassname="active"
-            className="contact-link"
-            to="contact"
-        >
-            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-        </NavLink>
-    </nav>
-    <ul>
-        <li>
-            <a
-                target= "_blank"
-                rel= "noreferrer"
-                href="https://github.com/"
+            <NavLink
+                exact="true"
+                activeclassname="active"
+                to="/"
             >
-            <FontAwesomeIcon icon={faGithub}  color="#4d4d4e" />
-            </a>
-        </li>
-        <li>
-            <a
-                target= "_blank"
-                rel= "noreferrer"
-                href="https://www.linkedin.com/"
+                <FontAwesomeIcon icon={faHome} color="4d4d4d" />
+            </NavLink>
+            <NavLink
+                exact="true"
+                activeclassname="active"
+                className="about-link"
+                to="about"
             >
-            <FontAwesomeIcon icon={faLinkedin}  color="#4d4d4e" />
-            </a>
-        </li>
-        <li>
-            <a
-                target= "_blank"
-                rel= "noreferrer"
-                href="https://www.youtube.com/"
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink
+                exact="true"
+                activeclassname="active"
+                className="contact-link"
+                to="contact"
             >
-            <FontAwesomeIcon icon={faYoutube}  color="#4d4d4e" />
-            </a>
-        </li>
-    </ul>
-</div>  
-)
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/"
+                >
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/"
+                >
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.youtube.com/"
+                >
+                    <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
+    </div>
+);
 
-export default Sidbar;
+export default Sidebar;
